e8fbeff23cb6fd478c0ee7dc9be8e1778197b02d
EliotAndres <eliota@bamlab.fr>
2015-09-03 11:47:29 +0200

    Removed phantom element and better focus

--- src/android/LibraryProject/src/com/google/zxing/client/android/CaptureActivity.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/CaptureActivity.java
@@ -91,7 +91,7 @@ public final class CaptureActivity extends Activity implements SurfaceHolder.Cal
 
   private static final String TAG = CaptureActivity.class.getSimpleName();
 
-  private static final long DEFAULT_INTENT_RESULT_DURATION_MS = 1500L;
+  private static final long DEFAULT_INTENT_RESULT_DURATION_MS = 0L;
   private static final long BULK_MODE_SCAN_DELAY_MS = 1000L;
 
   private static final String PACKAGE_NAME = "com.google.zxing.client.android";
